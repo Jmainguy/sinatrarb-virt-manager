4b1e9d9a592f14ddd8f2543cbfbc13b67eaf126b
Jonathan Mainguy <jon@soh.re>
2015-07-03 17:30:28 -0400

    Removed hard coded vm names, added /list, now only works with valid vm names

--- app.rb
+++ app.rb
@@ -3,21 +3,30 @@ require 'sinatra'
 set :port, 8080
 set :bind, '78.46.36.43'
 
-
-get '/' do
-  status = %x[virsh list --all | awk '/centos7.soh.re/ {print $3," ",$4}']
-  erb :virt, :locals => {:status => status}
+get '/list' do
+  status = %x[virsh list --all | awk '!/Id    Name                           State/ && !/----/ '].lines.map(&:chomp)
+  status.reject! { |c| c.empty? }
+  erb :list, :locals => {:status => status}
 end
 
-post '/' do
+get '/:name' do |vm|
+  status = %x[virsh list --all | awk '/#{vm}/ {print $3," ",$4}']
+  if status != ""
+    erb :virt, :locals => {:status => status,:vm => vm}
+  else
+    "<h1><center>This VM Does not Exist</h1></center>"
+  end
+end
+
+post '/action' do
   if "#{params[:action]}" == "restart"
-    %x[virsh destroy centos7.soh.re]
-    %x[virsh start centos7.soh.re]
+    %x[virsh destroy #{params[:vm]}]
+    %x[virsh start #{params[:vm]}]
   elsif "#{params[:action]}" == "start" || "#{params[:action]}" == "destroy"
-    %x[virsh "#{params[:action]}" centos7.soh.re]
+    %x[virsh "#{params[:action]}" #{params[:vm]}]
   else
     "Stop trying to be a hacker jimmy, its never gonna happen for you"
     sleep 10
   end
-  redirect "/"
+  redirect "/#{params[:vm]}"
 end
